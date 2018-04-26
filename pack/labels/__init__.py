b49cb69933a4f0fea224810b5f1b8d8e90177406
Aman <amankapur91@gmail.com>
2018-04-26 15:39:29 +0530

    added support for variable page height labels, and more util functions

--- pack/labels/__init__.py
+++ pack/labels/__init__.py
@@ -1,15 +1,13 @@
 import pdfkit
 import os
 import string
+import math
 
 from pack.utils import replace_all
 
-my_path = os.path.abspath(os.path.dirname(__file__))
-base_html = open(my_path + "/view/base.html", 'r').read()
-page_html = open(my_path + "/view/page.html", 'r').read()
-
 
 def g_pdf(fname, html, custom_options={}):
+
 	options = {
 		'page-width': 165.4,
 		'page-height': 165.4,
@@ -18,13 +16,58 @@ def g_pdf(fname, html, custom_options={}):
 		'margin-top': '0',
 		'margin-left': '0',
 		'margin-right': '0',
-		'dpi': 300
+		'dpi': 300,
+		'javascript-delay': 2500
 	}
+
 	for k,v in custom_options.iteritems():
-		options[k] = v
+		if k in options:
+			options[k] = v
+
 	pdfkit.from_string(html, fname, options=options)
 
-def make_labels(data, label_template):
+def make_labels(label_data, label_template, output_file, options={}, debug=False):
+	defaults = {
+		'custom_style': None,
+		'max_label_per_file': len(label_data)+1,
+		'duplicate': False,
+		'page-height': 165.4
+	}
+
+	for k,v in defaults.iteritems():
+		if k not in options:
+			options[k] = v
+
+	data = None
+	if options['duplicate']:
+		data = []
+		for l_d in label_data:
+			for i in range(2):
+				data.append(l_d)
+	else:
+		data = label_data
+
+	MAX_FILE_SIZE = options['max_label_per_file']
+
+	file_count = int(math.ceil(len(data)/float(MAX_FILE_SIZE)))
+	for i in range(file_count):
+		start = i*MAX_FILE_SIZE
+		end = min((i+1)*MAX_FILE_SIZE, len(data))
+		out_file = None
+		if file_count > 1:
+			out_file = output_file.split('.pdf')[0] + '_PART_%s.pdf' % str(i+1)
+		else:
+			out_file = output_file
+		print 'Creating %s labels : %s ' % (str(end-start), out_file)
+		_create_html_labels(data[start:end], label_template, out_file, options, debug)
+
+
+def _create_html_labels(data, label_template, output_file, options, debug):
+	my_path = os.path.abspath(os.path.dirname(__file__))
+	base_html = open(my_path + "/view/base.html", 'r').read()
+	base_html = base_html.replace("#PAGE_HEIGHT", str(options['page-height']))
+	page_html = open(my_path + "/view/page.html", 'r').read()
+
 	pages = ""
 	for d in data:
 		page_data = {
@@ -32,11 +75,23 @@ def make_labels(data, label_template):
 		}
 		r_data = {}
 		for k,v in d.iteritems():
-			r_data['#'+str(k)] = str(v)
+			r_data['#'+str(k)] = str(v or "")
 		page_data["#label"] = replace_all(label_template, r_data)
 
 		pages += replace_all(page_html, page_data)
 
 	render_html = base_html.replace("#pages", pages)
-	print "Starting pdf creation : %s " % out_file
-	g_pdf(out_file, render_html)
+
+	custom_style = ""
+	if 'custom_style' in options.keys():
+		custom_style = options['custom_style']
+
+	render_html = render_html.replace("#custom_style", custom_style)
+
+	if debug:
+		print 'Output debug file created debug_html.html'
+		debug = open('debug_html.html', 'w')
+		debug.write(render_html)
+		debug.close()
+
+	g_pdf(output_file, render_html, options)
--- pack/utils/__init__.py
+++ pack/utils/__init__.py
@@ -8,176 +8,206 @@ from openpyxl.styles import Alignment, Font, Border, Side
 
 from pprint import pprint
 
+import os
+
+
+# needs ssconvert installed
+def convert_to_xls(in_path):
+	os.system("ssconvert %s %s" % (in_path, in_path[:-1]))
+
+def get_min(arr_of_dicts, key):
+	min_val = float("inf")
+
+	for d in arr_of_dicts:
+		min_val = min(min_val, d[key])
+	return min_val
+
+def get_max(arr_of_dicts, key):
+	max_val = float("-inf")
+
+	for d in arr_of_dicts:
+		max_val = max(max_val, d[key])
+	return max_val
+
+
 #keymap is a has str->str
 # it maps keys of the in_doc merge fields to data keys/funcs
 # data is [{..},{...}] format
 def my_merge(in_doc_path, out_doc_path, data, key_map=None):
-  mm_data = []
-  if key_map:
-    for d in data:
-      mm_d = {}
+	mm_data = []
+	if key_map:
+		for d in data:
+			mm_d = {}
 
-      for mm_key, d_key in key_map.iteritems():
-        v = ''
-        if callable(d_key):
-          v = d_key(d)
-        else:
-          v = d[d_key]
+			for mm_key, d_key in key_map.iteritems():
+				v = ''
+				if callable(d_key):
+					v = d_key(d)
+				else:
+					v = d[d_key]
 
-        mm_d[mm_key] = v
+				mm_d[mm_key] = v
 
-      mm_data.append(mm_d)
-  else:
-    mm_data = data
+			mm_data.append(mm_d)
+	else:
+		mm_data = data
 
-  formatted_d = [{},{}]
-  for d in mm_data:
-    for k,v in d.iteritems():
-      d[k] = str(v)
-    formatted_d.append(d)
+	formatted_d = [{},{}]
+	for d in mm_data:
+		for k,v in d.iteritems():
+			d[k] = str(v)
+		formatted_d.append(d)
 
-  MAX_SIZE_PER_FILE = 5000
-  with MailMerge(in_doc_path) as document:
-    document.merge_pages(formatted_d[:MAX_SIZE_PER_FILE])
-    document.write(out_doc_path)
+	MAX_SIZE_PER_FILE = 5000
+	with MailMerge(in_doc_path) as document:
+		document.merge_pages(formatted_d[:MAX_SIZE_PER_FILE])
+		document.write(out_doc_path)
 
 
 CELL_STYLES = {
-  'HEADER':{
-    'alignment': Alignment(horizontal='center', vertical='center', wrap_text="true"),
-    'font': Font(bold=True),
-    'border':  Border(left=Side(style='thin'),
-                       right=Side(style='thin'),
-                       top=Side(style='thin'),
-                       bottom=Side(style='thin'))
-  },
-  'TEXT':{
-    'alignment': Alignment(horizontal='center', vertical='center'),
-    'border':  Border(left=Side(style='thin'),
-                       right=Side(style='thin'),
-                       top=Side(style='thin'),
-                       bottom=Side(style='thin'))
-  },
-  'TITLE':{
-    'alignment': Alignment(horizontal='center', vertical='center'),
-    'font': Font(bold=True, size=15)
-  }
+	'HEADER':{
+		'alignment': Alignment(horizontal='center', vertical='center', wrap_text="true"),
+		'font': Font(bold=True),
+		'border':  Border(left=Side(style='thin'),
+											 right=Side(style='thin'),
+											 top=Side(style='thin'),
+											 bottom=Side(style='thin'))
+	},
+	'TEXT':{
+		'alignment': Alignment(horizontal='center', vertical='center'),
+		'border':  Border(left=Side(style='thin'),
+											 right=Side(style='thin'),
+											 top=Side(style='thin'),
+											 bottom=Side(style='thin'))
+	},
+	'TITLE':{
+		'alignment': Alignment(horizontal='center', vertical='center'),
+		'font': Font(bold=True, size=15)
+	}
 }
 
 def insert_row(sheet, data, row_num, styles={}):
-  col_idx = 1
-  for d in data:
-    cell = sheet.cell(row=row_num, column=col_idx, value=d or "")
-    if "alignment" in styles.keys():
-      cell.alignment = styles["alignment"]
-    if "font" in styles.keys():
-      cell.font = styles["font"]
-    if 'border' in styles.keys():
-      cell.border = styles['border']
-    col_idx +=1
+	col_idx = 1
+	for d in data:
+		cell = sheet.cell(row=row_num, column=col_idx, value=d or "")
+		if "alignment" in styles.keys():
+			cell.alignment = styles["alignment"]
+		if "font" in styles.keys():
+			cell.font = styles["font"]
+		if 'border' in styles.keys():
+			cell.border = styles['border']
+		col_idx +=1
 
 
 def copy_style(new_cell, cell):
-  new_cell.font = copy(cell.font)
-  new_cell.border = copy(cell.border)
-  new_cell.fill = copy(cell.fill)
-  new_cell.number_format = copy(cell.number_format)
-  new_cell.protection = copy(cell.protection)
-  new_cell.alignment = copy(cell.alignment)
+	new_cell.font = copy(cell.font)
+	new_cell.border = copy(cell.border)
+	new_cell.fill = copy(cell.fill)
+	new_cell.number_format = copy(cell.number_format)
+	new_cell.protection = copy(cell.protection)
+	new_cell.alignment = copy(cell.alignment)
 
 def display_from_to(from_val, to_val):
-  if from_val is None or to_val is None:
-    return None
-  if from_val == to_val:
-    return str(from_val)
-  else:
-    return str(from_val) + ' - ' + str(to_val)
+	if from_val is None or to_val is None:
+		return None
+	if from_val == to_val:
+		return str(from_val)
+	else:
+		return str(from_val) + ' - ' + str(to_val)
 
 def display_time(t):
-  a = ' AM'
-  if t.hour > 12:
-    a = ' PM'
-  return str(t.hour%12) + ':' + str(t.minute) + a
+	a = ' AM'
+	if t.hour > 12:
+		a = ' PM'
+	return str(t.hour%12) + ':' + str(t.minute) + a
 
 def display_month(m):
-  if 'MAR' in m :
-    return 'MARCH'
-  return m
+	if 'MAR' in m :
+		return 'MARCH'
+	return m
 
 def round_up(x, n):
-  n = float(n)
-  return int(math.ceil(x / n) * n)
+	n = float(n)
+	return int(math.ceil(x / n) * n)
 
 def get_string_sub_code(code):
-  c = str(code)
-  if len(c) != 2:
-    c = '0'+ c
-  return c
+	c = str(code)
+	if len(c) != 2:
+		c = '0'+ c
+	return c
 
 def zerofy(n):
-  if n < 10:
-    return '0' + str(n)
-  else:
-    return str(n)
+	if n < 10:
+		return '0' + str(n)
+	else:
+		return str(n)
 
 def time_plus(time, timedelta):
-    start = datetime.datetime(
-        2000, 1, 1,
-        hour=time.hour, minute=time.minute, second=time.second)
-    end = start + timedelta
-    return end.time()
+		start = datetime.datetime(
+				2000, 1, 1,
+				hour=time.hour, minute=time.minute, second=time.second)
+		end = start + timedelta
+		return end.time()
 
 
 
 def split_arr_n(arr, x):
-  n = int(math.ceil(len(arr)/float(x)))
+	n = int(math.ceil(len(arr)/float(x)))
 
-  new_arr = []
-  for i in range(0, n):
-    for j in range(0, x):
-      new_index = i+j*n
-      if new_index < len(arr):
-        new_arr.append(arr[new_index])
-      # else:
-      #   new_arr.append(None)
+	new_arr = []
+	for i in range(0, n):
+		for j in range(0, x):
+			new_index = i+j*n
+			if new_index < len(arr):
+				new_arr.append(arr[new_index])
+			# else:
+			#   new_arr.append(None)
 
-  return new_arr
+	return new_arr
 
 def sort_by(items, columns):
-  return multikeysort(items, columns)
+	return multikeysort(items, columns)
 
 def multikeysort(items, columns):
-  from operator import itemgetter
-  comparers = [ ((itemgetter(col[1:].strip()), -1) if col.startswith('-') else (itemgetter(col.strip()), 1)) for col in columns]
-  def comparer(left, right):
-      for fn, mult in comparers:
-          result = cmp(fn(left), fn(right))
-          if result:
-              return mult * result
-      else:
-          return 0
-  return sorted(items, cmp=comparer)
+	from operator import itemgetter
+	comparers = [ ((itemgetter(col[1:].strip()), -1) if col.startswith('-') else (itemgetter(col.strip()), 1)) for col in columns]
+	def comparer(left, right):
+			for fn, mult in comparers:
+					result = cmp(fn(left), fn(right))
+					if result:
+							return mult * result
+			else:
+					return 0
+	return sorted(items, cmp=comparer)
 
-def group_by(input_data, keys):
-  def _group(datum, k):
-    data = {}
-    for d in datum:
-      if d[k] not in data.keys():
-        data[d[k]] = []
-      data[d[k]].append(d)
-    return data
+def group_by(input_data, keys, expect_single=False):
+	def _group(datum, k):
+		data = {}
+		for d in datum:
+			if d[k]:
+				if d[k] not in data.keys():
+					if expect_single:
+						data[d[k]] = d
+					else:
+						data[d[k]] = [d]
+				elif expect_single:
+					raise ValueError('Expected single item in group_by')
+				else:
+					data[d[k]].append(d)
+		return data
 
-  if type(keys) in [str, unicode]:
-    return _group(input_data, keys)
+	if type(keys) in [str, unicode]:
+		return _group(input_data, keys)
 
-  data = _group(input_data, keys[0])
-  for k, datum in data.iteritems():
-    data[k] = _group(datum, keys[1])
+	data = _group(input_data, keys[0])
+	for k, datum in data.iteritems():
+		data[k] = _group(datum, keys[1])
 
-  return data
+	return data
+		
 
 def replace_all(string, d):
-  s = string
-  for k,v in d.items():
-    s = s.replace(k, str(v))
-  return s
+	s = string
+	for k,v in d.items():
+		s = s.replace(k, str(v))
+	return s
